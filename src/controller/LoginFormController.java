bc6a79e3b1a0e531964eb48bf7a7f27200ae31a9
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-07 02:39:40 +0530

    Login UI controller class implemented

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,11 +2,20 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXTextField txtUsername;
 
-    public void btnLogin(ActionEvent actionEvent) {
-
+    public void btnLogin(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("../view/ClientForm.fxml"))));
+        stage.setTitle(txtUsername.getText());
+//        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
     }
 }
