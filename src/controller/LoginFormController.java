b73a958815ca9965918c8627492e4a3beebd1769
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-07 02:35:59 +0530

    Introduced Client UI with its respective controller class

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtUsername;
+
+    public void btnLogin(ActionEvent actionEvent) {
+
+    }
 }
