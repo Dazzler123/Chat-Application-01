d5cec12e9d38f93363a007b850dbb39201275fff
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-07 15:46:03 +0530

    Implemented Client UI and its controller class

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class ClientFormController {
+    public Label lblUsername;
+    public TextArea txtMsgArea;
+    public TextField txtTypeMessage;
+
+    public void btnLogout(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void sendMessageIcon(MouseEvent mouseEvent) {
+    }
+
+    public void emojiIcon(MouseEvent mouseEvent) {
+    }
+
+    public void cameraIcon(MouseEvent mouseEvent) {
+    }
 }
