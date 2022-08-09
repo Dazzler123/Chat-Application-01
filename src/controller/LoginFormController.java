9982ffcd65dcb2d75f98f584e874522a6f05b959
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-10 01:09:40 +0530

    UI titles updated

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,9 +2,6 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import util.UINavigation;
 
 import java.io.IOException;
@@ -16,17 +13,16 @@ public class LoginFormController {
         //load client 1
         if (txtUsername.getText().equals("Dazzler")) {
             //load client ui
-            UINavigation.loadUI("ClientForm1", txtUsername.getText());
+            UINavigation.loadUI("ClientForm1", "Dazzler");
 
             //clear text
             txtUsername.clear();
         }
 
-
         //load client 2
         if (txtUsername.getText().equals("Peter")) {
             //load client ui
-            UINavigation.loadUI("ClientForm2", txtUsername.getText());
+            UINavigation.loadUI("ClientForm2", "Peter");
 
             //clear text
             txtUsername.clear();
@@ -35,7 +31,7 @@ public class LoginFormController {
         //load client 3
         if (txtUsername.getText().equals("Marky")) {
             //load client ui
-            UINavigation.loadUI("ClientForm3", txtUsername.getText());
+            UINavigation.loadUI("ClientForm3", "Marky");
 
             //clear text
             txtUsername.clear();
