eb23cfab7c7acb8ffa03b0bfcfda6b9bcc894a93
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-09 13:29:26 +0530

    Set username label

--- src/controller/ClientForm2Controller.java
+++ src/controller/ClientForm2Controller.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ClientForm2Controller {
+    static String username;
     public TextArea txtMsgArea;
     public TextField txtTypeMessage;
     public ImageView sendMessageIcon;
@@ -28,6 +29,9 @@ public class ClientForm2Controller {
     String message = "";
 
     public void initialize() {
+        //set username
+        lblUsername.setText(username);
+
         new Thread(() -> {
             try {
                 socket = new Socket("localhost", PORT);
