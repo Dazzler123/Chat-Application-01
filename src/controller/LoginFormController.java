51224c526c07f8369e208c9656b8252861517a81
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-09 13:30:36 +0530

    Removed set username

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -14,9 +14,6 @@ public class LoginFormController {
     public void btnLogin(ActionEvent actionEvent) throws IOException {
         //load client 1
         if (txtUsername.getText().equals("Dasindu")) {
-            //set username label
-            ClientFormController.username = txtUsername.getText();
-
             //load client ui
             Stage stage = new Stage();
             stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("../view/ClientForm.fxml"))));
