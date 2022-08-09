4c84e1bba3a2e74c82e0f300538f81504fe50607
Dazzler123 <dasinduhewagamage@gmail.com>
2022-08-09 13:22:36 +0530

    ClientFormController updated

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -12,119 +12,54 @@ import java.io.*;
 import java.net.Socket;
 
 public class ClientFormController {
-
     static String username;
     public Label lblUsername;
     public TextArea txtMsgArea;
     public TextField txtTypeMessage;
 
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-    private String usrname;
+    final int PORT = 1232;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
 
-//    final int PORT = 5000;
-//    Socket socket;
-//    DataInputStream dataInputStream;
-//    DataOutputStream dataOutputStream;
-
-//    String message = "";
-
-    public ClientFormController(Socket socket, String usrname){
-        try {
-            this.socket = socket;
-            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.usrname = usrname;
-        } catch (IOException e){
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        }
-    }
+    String message = "";
 
     public void initialize(){
         //set username
         lblUsername.setText(username);
 
-//        new Thread(() -> {
-//            try {
-//                socket = new Socket("localhost", PORT);
-//
-//                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-//                dataInputStream = new DataInputStream(socket.getInputStream());
-//
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost", PORT);
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
 //                while (!message.equals("exit")) {
 //                    message = dataInputStream.readUTF();
-//                    txtMsgArea.appendText("\nServer : " + message);
+//                    txtTextArea.appendText("\nServer : " + message);
 //                }
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }).start();
 
-    }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
-    public void btnLogout(ActionEvent actionEvent) {
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.close();
     }
 
     public void sendMessageIcon(MouseEvent mouseEvent) throws IOException {
-//        dataOutputStream.writeUTF(txtTypeMessage.getText().trim());
-//        dataOutputStream.flush();
-        try {
-            bufferedWriter.write(usrname);
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-
-            while (socket.isConnected()){
-                String typedMessage = txtTypeMessage.getText().trim();
-                bufferedWriter.write(username + ": " + typedMessage);
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-            }
-        } catch (IOException e){
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        }
-    }
-
-    public void listenForMessage(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String otherMessages;
-
-                while (socket.isConnected()) {
-                    try {
-                        otherMessages = bufferedReader.readLine();
-                        txtMsgArea.appendText(otherMessages);
-                    } catch (IOException e){
-                        closeEverything(socket, bufferedReader, bufferedWriter);
-                    }
-                }
-            }
-        }).start();
-    }
-
-    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
-        try {
-            if (bufferedReader != null) {
-                bufferedReader.close();
-            }
-            if (bufferedWriter != null) {
-                bufferedWriter.close();
-            }
-            if (socket != null) {
-                socket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        dataOutputStream.writeUTF(txtTypeMessage.getText().trim());
+        dataOutputStream.flush();
     }
 
     public void emojiIcon(MouseEvent mouseEvent) {
     }
 
     public void cameraIcon(MouseEvent mouseEvent) {
+    }
+
+    public void btnLogout(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
